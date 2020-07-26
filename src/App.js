6fc8620c5c6048bde5d7af96ed1f0714bf3a8b37
Mhret Moges <andreasjhon488@gmail.com>
2020-07-26 15:50:18 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Heading } from './components/Heading';
+import { Loading } from './components/Loading';
+import { UnsplashImage } from './components/UnsplashImage';
 
+import axios from 'axios';
+import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+const WrapperImage = styled.section`
+  max-width: 70rem;
+  margin: 4rem auto;
+  display: grid;
+  grid-gap: 1em;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-auto-rows: 300px;
+`;
 function App() {
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    fetchImages();
+  }, []);
+  const fetchImages = () => {
+    const apiRoot = 'https://api.unsplash.com';
+    const accessKey = process.env.REACT_APP_ACCESSKEY;
+    axios
+      .get(`${apiRoot}/photos/random?client_id=${accessKey}&count=10`)
+      .then((res) => {
+        setImages([...images, ...res.data]);
+        console.log(res.data);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Heading />
+      <GlobalStyle />
+      <InfiniteScroll
+        dataLength={images.length}
+        next={fetchImages}
+        hasMore={true}
+        loader={<Loading />}>
+        <WrapperImage>
+          {images.map((image) => (
+            <UnsplashImage url={image.urls.thumb} key={image.id} />
+          ))}
+        </WrapperImage>
+      </InfiniteScroll>
     </div>
   );
 }
